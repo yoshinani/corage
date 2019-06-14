65ee2c0de22f63dd11b85d9f77d4c6fcf3a5d8de
yutanakano <yutanakano395@icloud.com>
2019-06-14 09:50:09 +0900

    :iphone: トップページで使うlayoutを適用

---
+++ app/layouts/main.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <div class="sf-site-all">
+      <Hero class="sf-site-content" />
+      <Shimoza />
+    </div>
+  </section>
+</template>
+
+<script>
+import Hero from '~/components/Hero.vue'
+import Shimoza from '~/components/Shimoza.vue'
+
+export default {
+  components: {
+    Hero,
+    Shimoza
+  }
+}
+</script>
+
+<style>
+.sf-site-all {
+  min-height: 100vh; /* 全体の高さを最低でもビューポートの100%にする */
+  display: flex; /* 子クラスを横並びにする */
+  flex-direction: column; /* 子クラスの横並びの方向を縦にする */
+}
+.sf-site-content {
+  flex: 1; /* flexに1つだけ数値を指定するとその要素は伸びる */
+}
+/* NavbarとBottomにはflexを指定しないので伸びない */
+/* 結果としてページファイルの中味だけが伸びて、ビューポート100%を実現 */
+/* Bottomは下に貼り付く。スティッキーフッターを実現 */
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -29,7 +29,8 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
-  }
+  },
+  layout: 'main'
 }
 </script>
 
