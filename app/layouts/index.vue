6f92807bd2e3a944cb358a60da19bcb2f7018d7a
raichel <yutanakano395@icloud.com>
2019-06-16 01:45:32 +0900

    トップページのデザイン作成 (#14)
    * :iphone: トップページで使うHeaderを作成
    
    * :iphone: トップページで使うlayoutを適用
    
    * :truck: URLのリソース名とファイル名を一致させた
    
    * :recycle: footerのcssをリファクタ
    
    * :truck: pageとlayoutの名称を統一
    
    URLとページとレイアウトの名称を統一することで保守性を上げたかった

---
+++ app/layouts/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <div>
+      <Hero />
+      <Shimoza />
+    </div>
+  </section>
+</template>
+
+<script>
+import Hero from '~/components/Hero.vue'
+import Shimoza from '~/components/Shimoza.vue'
+
+export default {
+  components: {
+    Hero,
+    Shimoza
+  }
+}
+</script>
