3a3dec5afb24c8a0159b907d1f7d1612dcf523de
yutanakano <yutanakano395@icloud.com>
2019-07-02 01:55:18 +0900

    :sparkles: ログイン機能を追加

---
+++ app/pages/auth/account.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <!-- ログイン中に表示される画面 -->
+    <div v-if="isAuthenticated">
+      {{ user.email }}でログイン中です<br />
+      <button @click="logout">ログアウト</button><br />
+      <a href="/home">メンバーページへ</a>
+    </div>
+    <!-- ログインしていない時に表示される画面 -->
+    <div v-else>
+      メール<br />
+      <input v-model="email" type="text" /><br />
+      パスワード<br />
+      <input v-model="password" type="password" /><br />
+      <button @click="login">ログイン</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import firebase from '~/plugins/firebase'
+import { mapActions, mapState, mapGetters } from 'vuex'
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  computed: {
+    ...mapState(['user']),
+    ...mapGetters(['isAuthenticated'])
+  },
+  mounted() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setUser(user)
+    })
+  },
+  methods: {
+    ...mapActions(['setUser']),
+    login() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(user => {
+          // ログインしたら飛ぶページを指定
+          // this.$router.push("/home")
+        })
+        .catch(error => {
+          alert(error)
+        })
+    },
+    logout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.setUser(null)
+        })
+        .catch(error => {
+          alert(error)
+        })
+    }
+  }
+}
+</script>
