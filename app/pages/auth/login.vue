a890093464d55ca2d291a705736a45d96a54f2a7
yutanakano <yutanakano395@icloud.com>
2019-07-07 01:28:19 +0900

    :art: homeページを作成

--- app/pages/auth/login.vue
+++ app/pages/auth/login.vue
@@ -1,19 +1,10 @@
 <template>
   <div>
-    <!-- ログイン中に表示される画面 -->
-    <div v-if="isAuthenticated">
-      {{ user.email }}でログイン中です<br />
-      <button @click="logout">ログアウト</button><br />
-      <a href="/home">ホームへ</a>
-    </div>
-    <!-- ログインしていない時に表示される画面 -->
-    <div v-else>
-      メール<br />
-      <input v-model="email" type="text" /><br />
-      パスワード<br />
-      <input v-model="password" type="password" /><br />
-      <button @click="login">ログイン</button>
-    </div>
+    メール<br />
+    <input v-model="email" type="text" /><br />
+    パスワード<br />
+    <input v-model="password" type="password" /><br />
+    <button @click="login">ログイン</button>
   </div>
 </template>
 
@@ -31,12 +22,6 @@ export default {
     ...mapState('auth', ['user']),
     ...mapGetters('auth', ['isAuthenticated'])
   },
-  mounted() {
-    firebase.auth().onAuthStateChanged(user => {
-      const { uid, email, displayName } = user
-      this.setUser({ uid, email, displayName })
-    })
-  },
   methods: {
     ...mapActions('auth', ['setUser']),
     login() {
@@ -44,19 +29,7 @@ export default {
         .auth()
         .signInWithEmailAndPassword(this.email, this.password)
         .then(user => {
-          // ログインしたら飛ぶページを指定
-          // this.$router.push("/home")
-        })
-        .catch(error => {
-          alert(error)
-        })
-    },
-    logout() {
-      firebase
-        .auth()
-        .signOut()
-        .then(() => {
-          this.setUser(null)
+          this.$router.push('/home')
         })
         .catch(error => {
           alert(error)
---
+++ app/pages/home.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- ログイン中に表示される画面 -->
+  <div>
+    {{ user.email }}でログイン中です<br />
+    <button @click="logout">ログアウト</button><br />
+    <a href="/home">ホームへ</a>
+  </div>
+</template>
+
+<script>
+import firebase from '~/plugins/firebase'
+import { mapActions, mapState, mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapState('auth', ['user']),
+    ...mapGetters('auth', ['isAuthenticated'])
+  },
+  mounted() {
+    firebase.auth().onAuthStateChanged(user => {
+      const { uid, email, displayName } = user
+      this.setUser({ uid, email, displayName })
+    })
+  },
+  methods: {
+    ...mapActions('auth', ['setUser']),
+    logout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.setUser(null)
+          this.$router.push('/')
+        })
+        .catch(error => {
+          alert(error)
+        })
+    }
+  }
+}
+</script>
