08b45e001b3419c583c615d18e60cea2efdaad79
yutanakano <yutanakano395@icloud.com>
2019-07-06 22:17:37 +0900

    :pencil: コメントを追加
    https://qiita.com/Takoyaki9/items/b6638fa1aec41464fdd1

--- app/store/auth.js
+++ app/store/auth.js
@@ -1,22 +1,30 @@
 export const strict = false
 
+// state:コンポーネントでいうdata
 export const state = () => ({
   user: null
 })
 
+// mutations:コンポーネントでいうmethod（と言うかsetter）
+// stateを唯一変更できるもの
 export const mutations = {
+  // vuexでは引数をpayloadと呼ぶっぽい
+  // payloadはオブジェクトにするべき（いっぱい入れれるし）
   setUser(state, payload) {
     state.user = payload
   }
 }
 
+// actionのコミットを使うことでミューテーションを呼び出す（コンポーネントには無い概念）
 export const actions = {
   setUser({ commit }, payload) {
     commit('setUser', payload)
   }
 }
 
+// getters:コンポーネントでいうcomputed的なもの
 export const getters = {
+  // isAuthenticatedを使用するgetter
   isAuthenticated(state) {
     return !!state.user
   }
