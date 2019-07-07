3f012af6085f98d2f1f223f424d1e23ea40be49a
yutanakano <yutanakano395@icloud.com>
2019-07-07 23:26:00 +0900

    :art: ログイン未ログインを見て振り分けるmiddlewareを追加

---
+++ app/middleware/authenticated.js
@@ -0,0 +1,11 @@
+import { fireauth } from '~/plugins/firebase'
+
+export default function({ route, store, redirect }) {
+  fireauth.onAuthStateChanged(user => {
+    if (user) {
+      store.dispatch('auth/setUser', user)
+    } else if (route.name === 'index') {
+      redirect('/')
+    } else if (route.name !== 'auth-login') redirect('/auth/login')
+  })
+}
