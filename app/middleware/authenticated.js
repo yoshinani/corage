197be071159a334448ef23b89689a2876a544f2a
yutanakano <yutanakano395@icloud.com>
2019-07-14 03:09:49 +0900

    :art: 非同期に変更し別タスクに設定したコードを削除
    https://in-field.atlassian.net/browse/CO-37

--- app/middleware/authenticated.js
+++ app/middleware/authenticated.js
@@ -1,11 +1,9 @@
 import { fireauth } from '~/plugins/firebase'
 
 export default function({ route, store, redirect }) {
-  fireauth.onAuthStateChanged(user => {
-    if (user) {
-      store.dispatch('auth/loginUser', user)
-    } else if (route.name === 'index') {
-      redirect('/')
-    } else if (route.name !== 'auth-login') redirect('/auth/login')
+  return new Promise((resolve, reject) => {
+    fireauth.onAuthStateChanged(user => {
+      resolve(user || false)
+    })
   })
 }
