72350ac82ab586a082ce1cc5eb789a1c78d7ed3e
yutanakano <yutanakano395@icloud.com>
2019-07-25 01:03:50 +0900

    :iphone: Auth関連のコンポーネントを取り扱うページを追加

---
+++ app/pages/auth.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container">
+    <div>
+      <signup />
+      <login />
+    </div>
+  </section>
+</template>
+
+<script>
+import Signup from '~/components/Auth/signup.vue'
+import Login from '~/components/Auth/login.vue'
+
+export default {
+  components: {
+    Signup,
+    Login
+  }
+}
+</script>
