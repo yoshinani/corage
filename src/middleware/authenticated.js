a39de0ecce59cca5872fb918081d4c49706b35cd
yutanakano <yutanakano395@icloud.com>
2019-10-11 17:20:03 +0900

    :construction: 認証Middlewareが正常に動作するように対応厨
    台風19号のため急遽コミットしなければならなくなった

--- src/middleware/authenticated.js
+++ src/middleware/authenticated.js
@@ -1,9 +1,34 @@
 import firebase from '~/plugins/firebase'
 
 export default function({ route, store, redirect }) {
-  return new Promise((resolve, reject) => {
-    firebase.auth().onAuthStateChanged(user => {
-      resolve(user || false)
+  console.log('処理前')
+  const storageItem = JSON.parse(sessionStorage.getItem('CorageAuthStatus'))
+  console.log(storageItem === null)
+  // sessionがnullの場合認証処理を行う
+  if (storageItem === null) {
+    const auth = new Promise((resolve, reject) => {
+      firebase.auth().onAuthStateChanged(user => {
+        resolve(user || false)
+      })
     })
-  })
+    auth.then(function() {
+      if (auth === undefined) {
+        redirect('/auth')
+      }
+      if (auth === false) {
+        redirect('/auth')
+      }
+      store.commit('auth/setSignInState', auth)
+    })
+  }
+  // {"auth":{"user":{}}} だった場合再認証させる
+  // ↑基本的にはログアウトのタイミングですべて消しておきたい
+  if (Object.keys(storageItem.auth.user).length) {
+    // ログアウトアクション叩きたい
+    // そのあとリダイレクトしたい
+  }
+  console.log('処理後')
+  // sessionを消すと以下はnullになる
+  console.log(sessionStorage.getItem('CorageAuthStatus'))
+  return store.state.auth.user
 }
